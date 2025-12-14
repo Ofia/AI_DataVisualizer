4829476bb4ef2f917bc67f6339ec1554f70623c2
Ofia <ofir08@gmail.com>
2025-12-14 17:29:31 +0100

    Fix: Add HuggingFaceProvider and CSVExtractor to __init__.py imports

--- ai_providers/__init__.py
+++ ai_providers/__init__.py
@@ -1,6 +1,7 @@
 # AI Providers Package
 from .base_provider import BaseProvider
 from .anthropic_provider import AnthropicProvider
+from .huggingface_provider import HuggingFaceProvider
 from .provider_factory import ProviderFactory
 
-__all__ = ['BaseProvider', 'AnthropicProvider', 'ProviderFactory']
+__all__ = ['BaseProvider', 'AnthropicProvider', 'HuggingFaceProvider', 'ProviderFactory']
--- data_extractors/__init__.py
+++ data_extractors/__init__.py
@@ -1,7 +1,8 @@
 # Data Extractors Package
 from .pdf_extractor import PDFExtractor
 from .excel_extractor import ExcelExtractor
+from .csv_extractor import CSVExtractor
 from .image_extractor import ImageExtractor
 from .extractor_factory import ExtractorFactory
 
-__all__ = ['PDFExtractor', 'ExcelExtractor', 'ImageExtractor', 'ExtractorFactory']
+__all__ = ['PDFExtractor', 'ExcelExtractor', 'CSVExtractor', 'ImageExtractor', 'ExtractorFactory']
